32ae11e8794e2488d84d448263116d63ca4f226c
Olalekan <lekass205@gmail.com>
2025-04-08 19:15:43 +0100

    remove toast from app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,13 @@
 import { computed, onMounted } from 'vue'
 import { RouterView, useRoute } from 'vue-router'
 import { useUserStore } from '@/stores/user.ts'
-import { useToast } from 'vue-toast-notification'
 
-const toast = useToast()
 const userStore = useUserStore()
 
 const route = useRoute()
 const layout = computed(() => route.meta.layout + '-pages' || 'public-pages')
 
 onMounted(() => {
-  toast.success('You did it!', {
-    position: 'top-right',
-    duration: 6000,
-  })
   if (localStorage.getItem('ACCESS_TOKEN')) {
     userStore.getProfile()
   }
