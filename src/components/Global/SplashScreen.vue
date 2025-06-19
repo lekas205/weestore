520b5f317756745644c5899f57452d8a0270ea98
Olalekan <lekass205@gmail.com>
2025-06-19 13:34:04 +0100

    implement splash screen

---
+++ src/components/Global/SplashScreen.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="tw-flex tw-items-center tw-justify-center tw-absolute tw-w-[100vw] tw-h-[100vh] tw-z-50 tw-bg-gradient-to-b tw-from-[#FA4A0C] tw-to-[#942C07]"
+  >
+    <img src="@/assets/images/svgs/logo-light.svg" />
+  </div>
+</template>
